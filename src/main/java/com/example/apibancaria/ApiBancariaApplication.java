8440fb884e19d248a32af6b82946462b6921f13d
omathcampos <matheusdcampos2005@gmail.com>
2024-12-10 14:47:41 -0300

    Commit inicial do projeto

---
+++ src/main/java/com/example/apibancaria/ApiBancariaApplication.java
@@ -0,0 +1,13 @@
+package com.example.apibancaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBancariaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiBancariaApplication.class, args);
+    }
+
+}
